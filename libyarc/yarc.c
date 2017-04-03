5abfb2071ec556f00d50d24fb19561e5e689e507
Marc-André Moreau <marcandre.moreau@gmail.com>
2017-04-03 17:06:36 -0400

    yarc: add API library

---
+++ libyarc/yarc.c
@@ -0,0 +1,84 @@
+
+#include <yarc/yarc.h>
+
+#ifdef YARC_LZ4
+extern int LZ4_decompress_safe(const char* source, char* dest, int compressedSize, int maxDecompressedSize);
+#else
+int LZ4_decompress_safe(const char* source, char* dest, int compressedSize, int maxDecompressedSize) { return 0; }
+#endif
+
+const unsigned char* yarc_bundle_find(yarc_bundle_t* bundle, const char* name, int* size)
+{
+	const unsigned char* data;
+	yarc_resource_t* resource;
+
+	resource = (yarc_resource_t*) bundle->resources;
+
+	while (resource->data)
+	{
+		if (!strcmp(name, resource->name))
+		{
+			data = *(resource->data);
+
+			if (size)
+				*size = *(resource->size);
+
+			return data;
+		}
+
+		resource++;
+	}
+
+	return 0;
+}
+
+bool yarc_bundle_open(yarc_bundle_t* bundle)
+{
+	int zstatus;
+	yarc_resource_t* resource;
+
+	if (bundle->zsize)
+	{
+		bundle->data = (unsigned char*) malloc(bundle->size);
+
+		if (!bundle->data)
+			return false;
+
+		zstatus = LZ4_decompress_safe(bundle->zdata, (char*) bundle->data, bundle->zsize, bundle->size);
+
+		if (zstatus != bundle->size)
+			return false;
+
+		resource = (yarc_resource_t*) bundle->resources;
+
+		while (resource->data)
+		{
+			*(resource->data) = &bundle->data[resource->offset];
+			resource++;
+		}
+	}
+
+	return true;
+}
+
+bool yarc_bundle_close(yarc_bundle_t* bundle)
+{
+	yarc_resource_t* resource;
+
+	if (bundle->zsize)
+	{
+		resource = (yarc_resource_t*) bundle->resources;
+
+		while (resource->data)
+		{
+			*(resource->data) = NULL;
+			resource++;
+		}
+
+		free((void*) bundle->data);
+		bundle->data;
+	}
+
+	return true;
+}
+
--- sample/sample.c
+++ sample/sample.c
@@ -1,73 +1,16 @@
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <yarc/yarc.h>
 
-#include "yarc.h"
-
+YARC_EXTERN_BUNDLE(sample)
 YARC_EXTERN(sample, colors_json);
 YARC_EXTERN(sample, squirrel_jpg);
-
-#ifdef YARC_LZ4
-extern int LZ4_decompress_safe(const char* source, char* dest, int compressedSize, int maxDecompressedSize);
-#else
-int LZ4_decompress_safe(const char* source, char* dest, int compressedSize, int maxDecompressedSize) { return 0; }
-#endif
-
-extern yarc_bundle_t yarc_sample_bundle;
-
-int yarc_bundle_open(yarc_bundle_t* bundle)
-{
-	int zstatus;
-	yarc_resource_t* resource;
-
-	if (bundle->zsize)
-	{
-		bundle->data = (unsigned char*) malloc(bundle->size);
-
-		if (!bundle->data)
-			return -1;
-
-		zstatus = LZ4_decompress_safe(bundle->zdata, (char*) bundle->data, bundle->zsize, bundle->size);
-
-		if (zstatus != bundle->size)
-			return -1;
-
-		resource = (yarc_resource_t*) bundle->resources;
-
-		while (resource->data)
-		{
-			*(resource->data) = &bundle->data[resource->offset];
-			resource++;
-		}
-	}
-
-	return 1;
-}
-
-int yarc_bundle_close(yarc_bundle_t* bundle)
-{
-	yarc_resource_t* resource;
-
-	if (bundle->zsize)
-	{
-		resource = (yarc_resource_t*) bundle->resources;
-
-		while (resource->data)
-		{
-			*(resource->data) = NULL;
-			resource++;
-		}
-
-		free((void*) bundle->data);
-		bundle->data;
-	}
-
-	return 1;
-}
 
 int main(int argc, char** argv)
 {
 	FILE* fp;
+	int size = 0;
+	uint8_t* data = NULL;
+	const char* name = "rfc4960.txt";
 
 	yarc_bundle_open(&yarc_sample_bundle);
 
@@ -78,6 +21,18 @@ int main(int argc, char** argv)
 	if (fp)
 	{
 		fwrite(YARC_DATA(sample, squirrel_jpg), 1, YARC_SIZE(sample, squirrel_jpg), fp);
+		fclose(fp);
+	}
+
+	const unsigned char* yarc_bundle_find(yarc_bundle_t* bundle, const char* name, int* size);
+	
+	data = (uint8_t*) yarc_bundle_find(&yarc_sample_bundle, name, &size);
+
+	if (data)
+	{
+		fp = fopen(name, "w+b");
+
+		fwrite(data, 1, size, fp);
 		fclose(fp);
 	}
 
